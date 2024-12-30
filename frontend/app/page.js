d5e2c6bbb77b1feb4bc1affb9e99f22355eb8d63
Alexander Collin Kollan <kollan.alex@gmail.com>
2024-12-30 14:06:58 +0200

    cicd

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-foreground">
-      <h1 className="text-4xl font-bold text-primary">Welcome to alexcoll.in!!</h1>
+      <h1 className="text-4xl font-bold text-primary">Welcome to alexcoll.in!!!</h1>
     </div>
   );
 }
